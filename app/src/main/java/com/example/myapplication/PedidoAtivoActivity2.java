de9a8fee2e5ad7a5578b826b735b0ded7d2c2fa4
AlexS1976 <damasioesilva@gmail.com>
2020-10-26 17:14:57 -0300

    Colocoção da segunda tela e link do botão da mainActyvit

---
+++ app/src/main/java/com/example/myapplication/PedidoAtivoActivity2.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PedidoAtivoActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido_ativo2);
+    }
+}
